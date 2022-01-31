4bc311f19828270f7ffedf09ed201451b69f4337
Harsini S R <harsini.ramalingam@gmail.com>
2022-01-31 21:24:41 +0530

    Feat: added api for status

--- routes/handler.js
+++ routes/handler.js
@@ -77,7 +77,8 @@ mondayRoutes.route("/monday/getData").get(async function (req, res) {
   const tokenData = await dbConnect.collection("mondaytoken").findOne(query);
   let token = tokenData.token;
   console.log(token);
-  const mondayQuery = "{ boards { name groups{title}} boards {  name  items{name  group { title } column_values {title text} } }}";
+  const mondayQuery =
+    "{ boards { name groups{title}} boards {  name  items{name  group { title } column_values {title text} } }}";
   const client = new GraphQLClient("https://api.monday.com/v2/", {
     headers: {
       "Content-Type": "application/json",
@@ -165,6 +166,49 @@ mondayRoutes.route("/monday/getData").get(async function (req, res) {
     finalData["formattedResult"] = result;
     return finalData;
   };
+});
+
+mondayRoutes.route("/monday/getStatus/update").post(async function (req, res) {
+  const dbConnect = dbo.getDb();
+  console.log(req);
+  dbConnect.collection("mondaystatus");
+  const referenceId = req.query.reference_id;
+  const query = { referenceId: referenceId };
+  let data = req.query.data;
+  const checkStatusData = await dbConnect
+    .collection("mondaystatus")
+    .findOne(query);
+  if (!checkStatusData) {
+    const schema = {
+      referenceId: referenceId,
+      date_added: new Date(),
+      data: data,
+    };
+    dbConnect.collection("mondaystatus").insertOne(schema);
+  } else {
+    const updates = {
+      $set: {
+        data: data,
+      },
+    };
+    dbConnect.collection("mondaystatus").updateOne(query, updates);
+  }
+});
+
+mondayRoutes.route("/monday/getStatus/get").get(async function (req, res) {
+  const dbConnect = dbo.getDb();
+  console.log(req);
+  const referenceId = req.query.reference_id;
+  const query = { referenceId: referenceId };
+  dbConnect.collection("mondaystatus").findOne(query, function (err, result) {
+    if (err || result == null) {
+      res.status(400).json({ message: "Error fetching status" });
+    } else {
+      res
+        .status(200)
+        .json({ message: "Status successfully retrieved", result: result });
+    }
+  });
 });
 
 module.exports = mondayRoutes;
