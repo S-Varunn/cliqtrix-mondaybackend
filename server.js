36a2c559aa7853b08641e5b665ce955afc871b26
S-Varunn <varunsureshoff@gmail.com>
2022-05-20 12:15:00 +0530

    Feat: Adding api

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const dbo = require("./connection/conn");
+const dbo = require("./connection/pasdbc");
 const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
@@ -12,7 +12,8 @@ app.use(
     extended: true,
   })
 );
-app.use(require("./routes/handler"));
+// app.use(require("./routes/handler"));
+app.use(require("./routes/pas"));
 
 dbo.connectToServer(function (err) {
   if (err) {
